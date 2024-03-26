035460e5d9bf69735b21dd239cb94b4372910918
Iryna Hrinchenko <i.orlovska63@gmail.com>
2024-03-26 10:04:21 +0200

    Changes for variable naming

--- src/main/java/org/example/homework3/ConvertDegrees.java
+++ src/main/java/org/example/homework3/ConvertDegrees.java
@@ -2,12 +2,12 @@ package org.example.homework3;
 
 public class ConvertDegrees {
     public static void main(String[] args) {
-        double Celsius = 12.5;
-        double Fahrenheit = (Celsius * 9/5) + 32;
-        double Kelvin = Celsius + 273.15;
+        double degreeCelsius = 12.5;
+        double degreeFahrenheit = (degreeCelsius * 9/5) + 32;
+        double degreeKelvin = degreeCelsius + 273.15;
 
-        System.out.println("Celsius: " + Celsius);
-        System.out.println("Fahrenheit: " + Fahrenheit);
-        System.out.println("Kelvin: " + Kelvin);
+        System.out.println("Celsius: " + degreeCelsius);
+        System.out.println("Fahrenheit: " + degreeFahrenheit);
+        System.out.println("Kelvin: " + degreeKelvin);
     }
 }
